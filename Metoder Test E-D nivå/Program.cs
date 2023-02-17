404365d980d7cf834087e5bb8842cdf43eb315b3
Adam <adamjamal0505@gmail.com>
2023-02-17 10:24:46 +0100

    Add project files.

---
+++ Metoder Test E-D nivå/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("skriv in bredden på din rektangel");
+        int tal1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("skriv in höjden på din rektangel");
+        int tal2 = int.Parse(Console.ReadLine());
+
+        int produkt = Area(tal1, tal2);
+        Console.WriteLine("Arean av din rektangel är " + produkt + "cm");// hgh
+
+    }
+    static int Area(int tal1, int tal2)
+    {
+        int produkt = tal1 * tal2;
+        return produkt;
+    }
+
+
+
+}
